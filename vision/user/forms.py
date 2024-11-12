465639ebf9731cdb71ab2ebe1de5591475d00406
net-ucc2024 <visin-netucc@outlook.com>
2024-11-11 21:45:50 -0500

    ##

--- vision/user/forms.py
+++ vision/user/forms.py
@@ -1,13 +1,11 @@
 # files.py
 from django import forms
 
-
-
-class RegistrationForm(forms.Form):
+class registro_form(forms.Form):
     primer_nombre = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Primer nombre'}))
     segundo_nombre = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Segundo nombre'}))
     apellido = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Apellido'}))
-    nombre_usuario = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Nombre de usuario"), error_messages={'invalid': ("El nombre de usuario debe contener solo letras,numeros y guiones bajos")})
-    email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Email"))
-    contraseña_1 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Contraseña"))
-    contraseña_2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Repita la contraseña"))
+    nombre_usuario = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=("Nombre de usuario"), error_messages={'invalid': ("El nombre de usuario debe contener solo letras,numeros y guiones bajos")})
+    email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=("Email"))
+    contraseña_1 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=("Contraseña"))
+    contraseña_2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=("Repita la contraseña"))
